7b8a2987a8bc965be0f1a94a564775cfbfe97622
babakmou <babakm77@yahoo.com>
2023-09-20 14:37:07 -0400

    Implementation of UI components finished.

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+const AddTechModal = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const onSubmit = () => {
+    if (firstName === '' || lastName === '') {
+      window.M.toast({ html: 'Please fill first name and last name fields!' });
+    } else {
+      console.log('modeal opened.');
+      setFirstName('');
+      setLastName('');
+    }
+  };
+
+  return (
+    <div id="add-tech-modal" className="modal">
+      <div className="modal-content">
+        <h4>New technician</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label htmlFor="firstName" className="active">
+              Log first name
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <label htmlFor="lastName" className="active">
+              Log last name
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect waves-light btn"
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AddTechModal;
